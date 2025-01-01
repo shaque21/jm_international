a1e5994220c326cd2f92fe8e5abc9c3efa3a7bbf
Samsul Haque <hshamsul894@gmail.com>
2025-01-01 21:12:23 +0600

    update customer
    update customer

---
+++ database/migrations/2025_01_01_144000_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('mobile',14);
+            $table->string('photo')->nullable();
+            $table->string('status')->default(1);
+            $table->string('slug')->unique();
+            $table->integer('role_id')->default(2);
+            $table->integer('emp_quantity');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
