a801414b5caaa0a2bf815e12a7b235af0cb50e9a
Samsul Haque <hshamsul894@gmail.com>
2025-01-24 22:12:05 +0600

    wip

--- resources/views/admin/main_header.blade.php
+++ resources/views/admin/main_header.blade.php
@@ -1,4 +1,4 @@
-<div class="main-header" data-background-color="purple">
+<div class="main-header" style="background-color: #0be881">
     <!-- Logo Header -->
     <div class="logo-header">
         
@@ -26,26 +26,8 @@
     <nav class="navbar navbar-header navbar-expand-lg">
         
         <div class="container-fluid">
-            {{-- <div class="collapse" id="search-nav">
-                <form class="navbar-left navbar-form nav-search mr-md-3">
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <button type="submit" class="btn btn-search pr-1">
-                                <i class="fa fa-search search-icon"></i>
-                            </button>
-                        </div>
-                        <input type="text" placeholder="Search ..." class="form-control">
-                    </div>
-                </form>
-            </div> --}}
             <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
-                {{-- <li class="nav-item toggle-nav-search hidden-caret">
-                    <a class="nav-link" data-toggle="collapse" href="#search-nav" role="button" aria-expanded="false" aria-controls="search-nav">
-                        <i class="fa fa-search"></i>
-                    </a>
-                </li> --}}
-
-
+               
                 <li class="nav-item dropdown hidden-caret">
                     <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
                         <div class="avatar-sm">
